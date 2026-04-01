8a2a4e64761c9b80f4382eb288f57f595a85f80d
Kayleighhhhh <kayleigh.vdham04@gmail.com>
2026-04-01 12:08:22 +0200

    limit #3

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.get('/instrumenten', async function (request, response) {
   if (soort) {
     params.set('filter[instrument][_eq]', soort)
   }
-  params.set('limit', 50)
+  params.set('limit', 20)
 
   // console.log(params.toString())
   //haal de link op om het filter achter te plakken
